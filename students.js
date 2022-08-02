a506ebb4521c3e43323667ab937420f54a9e9397
Dolsen0 <derek.olsen@gmail.com>
2022-08-02 11:53:31 -0400

    added css

--- students.js
+++ students.js
@@ -6,8 +6,23 @@ const PORT = 4001
 app.use(cors())
 app.use(express.json())
 
-app.get("/", (req, res) => {
-    res.send("ok")
+
+l2:38
+
+let students = ["bob","samantha","lily","grace","jim"]
+
+app.get("/",(req,res)=> {
+
+    let output = "<html><body><ul>" // starts
+    
+    //output = output + 'hello there html' //middle
+    for(let i = 0; i < students.length;i++) {
+        output = output + '<li><input type="checkbox">' + students[i] + '</li>'
+    }
+
+    output = output + "</ul></body></html>" //end
+
+    res.send(output)
 })
 
 app.listen(PORT, ()=>{
